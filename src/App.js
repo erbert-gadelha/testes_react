0f67ab129924ab928beebb4736482dfcfdd810b3
erbert-gadelha <ebgr@cin.ufpe.br>
2022-09-22 12:49:14 -0300

    parte vizual do pop_up data-qntd

--- src/App.js
+++ src/App.js
@@ -1,30 +1,95 @@
-import logo from './logo.svg';
 import React, { Component }  from 'react';
 import './App.css';
 
-/*function clique (a) {
-  window.alert("clicou" + a);
-}*/
+class InputNumber extends React.Component {
+  state = {
+    value: 0,
+  }
+
+  constructor() {
+    super();
+    
+    this.refresh = this.refresh.bind(this);
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+  }
+  
+  get value() {
+    return this.state.value;
+  }
+
+  refresh() {
+    this.setState({ value: this.value - 1 });
+  }
+
+  increment() {
+    const { max } = this.props;
+    
+    if (typeof max === 'number' && this.value >= max) return;
+    
+    this.setState({ value: this.value + 1 });
+  }
+
+  decrement() {
+    const { min } = this.props;
+    
+    if (typeof min === 'number' && this.value <= min) return;
+    
+    this.setState({ value: this.value - 1 });
+  }
+  
+  render() {
+    return (
+      <div className="input_number" style={this.props.style}>
+        <button type="button" onClick={this.decrement}>&minus;</button>
+        <span>{this.value}</span>
+        <button type="button" onClick={this.increment}>&#43;</button>     
+      </div>
+    )
+  }
+}
 
 
+
+function popup() {
+  let inputValue = 3;
+  let updateInputValue = (v) => {
+    inputValue += v;
+  }
+
+  return <div className='popup_bkg'>
+    <div className='popup_box'>
+        Escolha a data
+        <input
+            type="date"
+            placeholder="MM/DD/AAAA">
+        </input>
+        <br/>
+        Quantidade de sacos
+        <InputNumber min={0} max={100}/>
+        <br/>
+        <button>Submeter</button>
+    </div>
+  </div>;
+}
 
 
 function header() {
 
   let inputValue;
-  let clique = () => {  window.alert("pesquisar<\"" + inputValue + "\">"); };
+  let clique = () => {
+    window.alert("pesquisar<\"" + inputValue + "\">");
+    popup();
+  };
 
+  let abrirPopup = () => {
+
+  }
+  
   let updateInputValue = (evt) => {
     const val = evt.target.value;
     inputValue = val;
-    // ...       
-    //this.setState({
-    //  inputValue: val
-    //});
   }
-
-
-
 
   return (
     <div className="header">
@@ -53,6 +118,7 @@ function App() {
   return  <div>
 
               {header()}
+              {popup()}
 
 
 
